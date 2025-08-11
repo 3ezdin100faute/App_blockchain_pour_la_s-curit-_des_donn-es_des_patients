cf6c708d495b8c69ca96851d410b279af7566657
user <mohamedkhaled.tebourbi@esprit.tn>
2025-08-11 12:12:25 +0200

    first 1

---
+++ src/main/java/com/example/micro_user/Entity/User.java
@@ -0,0 +1,28 @@
+package com.example.micro_user.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    @Column(unique = true, nullable = false) // Empêche les doublons d'email
+    private String email;
+    private String password;
+    private String role;
+
+
+    // Exemple : "USER" ou "ADMIN"
+}
